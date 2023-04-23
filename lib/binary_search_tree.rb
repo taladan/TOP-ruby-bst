0ba72912fd1b14887980b60e90b6eb7e9ccbd406
D.J. Crosby <taladan@gmail.com>
2023-04-23 11:33:11 -0400

    fix: (#insert) implement working insert

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -17,56 +17,51 @@ class BinarySearchTree
     pretty_print(node.left_branch, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_branch
   end
   
-  def insert()
-
+  def insert(value, current_node=@root)
+    direction = value <=> current_node.object
+    
+    if current_node.leaf?
+      case direction
+      when -1
+        current_node.left_branch = create_node(value)
+        current_node.left_branch.root = current_node
+        output = current_node
+      when 0
+        output = current_node
+      when 1
+        current_node.right_branch = create_node(value)
+        current_node.right_branch.root = current_node
+        output = current_node
+      end
+      return output
+    end
+    
+    if value < current_node.object && current_node.left_branch.nil?
+      current_node.left_branch = create_node(value)
+      current_node.left_branch.root = current_node
+    elsif value < current_node.object && !current_node.left_branch.nil?
+      insert(value, current_node.left_branch)
+    elsif value > current_node.object && current_node.right_branch.nil?
+      current_node.right_branch = create_node(value)
+      current_node.right_branch.root = current_node
+    elsif value > current_node.object && !current_node.right_branch.nil?
+      insert(value, current_node.right_branch)
+    elsif value == current_node.object
+      return current_node  
+    end
   end
 
   private
 
-  require "pry-byebug"
-  def build_tree(array, start=0, stop=array.length)
-    # binding.pry
+  def build_tree(array, start=0, stop=array.length, current_node=@root)
     mid = (start + stop)/2
     return nil if start > stop
     root = create_node(array[mid])
-    root.left_branch = build_tree(array, start, mid-1)
-    root.right_branch = build_tree(array, mid+1, stop)
+    root.root = current_node
+    root.left_branch = build_tree(array, start, mid-1, root)
+    root.right_branch = build_tree(array, mid+1, stop, root)
     return root
   end
-
-  # def build_branch(array)
-  #   binding.pry
-  #   node = create_node(get_midpoint_value(array))
-  #   left = array[0..get_midpoint(array)]
-  #   right = array[get_midpoint(array) + 1..-1]
-  #   left_node = create_node(get_midpoint_value(left)) 
-  #   right_node = create_node(get_midpoint_value(right)) 
-  #   left_node.root = node
-  #   right_node.root = node
-  #   node.left_branch = build_branch(left)
-  #   node.right_branch = build_branch(right)
-  #   return node
-  # end
-
-  def get_midpoint_value(array)
-    return array[get_midpoint(array)]
-  end
-  
-  def get_midpoint(array)
-    return array.length/2
-  end
-
-  def create_child(node, current_node)
-    case node.value <=> current_node.value
-    when -1
-      current_node.left_branch = node
-    when 0
-      return nil
-    when 1
-      current_node.right_branch = node
-    end
-  end
-
 
   def create_node(object)
     Node.new(object)
