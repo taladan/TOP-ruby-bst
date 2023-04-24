02fb7f4c1be5e3fd0cb68944efaf34b34e033794
D.J. Crosby <taladan@gmail.com>
2023-04-24 14:56:42 -0400

    fix: rubocop cleanup of Node

--- lib/node.rb
+++ lib/node.rb
@@ -1,25 +1,34 @@
 # node.rb
 # This acts as a node for binary_search_tree algorithm
 
-class Node 
+class Node
   attr_accessor :object, :left_branch, :right_branch, :root
+
   def initialize(object)
-    @object = object 
+    @object = object
     @root = nil
     @left_branch = nil
     @right_branch = nil
   end
-  
-  def clear!  
+
+  def clear!
     @left_branch = nil
     @right_branch = nil
     @root = nil
     @object = nil
   end
-  
-  def leaf? 
+
+  def leaf?
     # have to have a root and no children to be a leaf
     return true if !@root.nil? && @left_branch.nil? && @right_branch.nil?
-    return false
+
+    false
+  end
+
+  def number_of_children
+    branches = [@left_branch, @right_branch]
+    children = 0
+    branches.each { |branch| children += 1 unless branch.nil? }
+    children
   end
 end
